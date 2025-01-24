eb83d780d5c118112c7da7667583ac2d41a11b6a
Daniel Njoroge <njorogedaniel088@gmail.com>
2025-01-24 23:47:33 +0300

    Site under construction

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import myimage from './assets/me.jpeg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App() {  
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img className="myimage" src={myimage} alt="My Photo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count iss {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        <h2>Site under construction!</h2>
+        contact: njorogedaniel088@gmail.com
       </p>
     </>
   )
