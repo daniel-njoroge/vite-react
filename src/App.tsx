817ecc7ba1f4582f04fdb513db4c7f026ddda9bf
Daniel Njoroge <126406280+daniel-njoroge@users.noreply.github.com>
2025-03-18 21:20:07 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import './App.css'
 function App() {  
   return (
     <>
-      <div>
-        <img className="myimage" src={myimage} alt="My Photo" />
-      </div>
-      
       <p className="read-the-docs">
         <h2>Site under construction!</h2>
         contact: njorogedaniel088@gmail.com
